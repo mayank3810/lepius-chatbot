49a496cf0c8b1d09b2daf0e48ec1e5d75aac2e9f
Mayank Gautam <mayankgautam2105@gmail.com>
2024-06-28 16:10:08 +0530

    build fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 
 function App() {
   const [inputMessage, setInputMessage] = useState("");
